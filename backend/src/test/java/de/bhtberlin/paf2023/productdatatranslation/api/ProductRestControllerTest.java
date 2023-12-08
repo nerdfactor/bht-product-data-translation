cf530947337ca81ef67a2ef2c0532a30626b0559
Daniel Klug <mail@nerdfactor.eu>
2023-12-08 07:43:36 +0100

    moved Rest controller tests in separate package. added mock responses from services to controller tests. extended the product rest controller tests.

---
+++ backend/src/test/java/de/bhtberlin/paf2023/productdatatranslation/api/ProductRestControllerTest.java
@@ -0,0 +1,137 @@
+package de.bhtberlin.paf2023.productdatatranslation.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.bhtberlin.paf2023.productdatatranslation.dto.ProductDto;
+import de.bhtberlin.paf2023.productdatatranslation.entity.Product;
+import de.bhtberlin.paf2023.productdatatranslation.service.ProductCrudService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.*;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ * Test for {@link Product} REST controller.
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductRestControllerTest {
+
+    private static final String API_PATH = "/api/products";
+
+    @Autowired
+    ObjectMapper jsonMapper;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * Mocked {@link ProductCrudService} in order to provide
+     * mock responses to the tested REST controller.
+     */
+    @MockBean
+    ProductCrudService productCrudService;
+
+    /**
+     * Check if {@link Product Products} can be listed.
+     */
+    @Test
+    void productsCanBeListed() throws Exception {
+        List<ProductDto> mockDtos = Arrays.asList(
+                createTestProduct(),
+                createTestProduct()
+        );
+        List<Product> mockEntities = mockDtos.stream().map(dto -> this.modelMapper.map(dto, Product.class)).toList();
+        Mockito.when(productCrudService.listAllProducts())
+                .thenReturn(mockEntities);
+
+        mockMvc.perform(get(API_PATH))
+                .andExpect(status().isOk())
+                .andExpect(content().json(jsonMapper.writeValueAsString(mockDtos)));
+    }
+
+    /**
+     * Check if a {@link Product} can be created.
+     */
+    @Test
+    void productCanBeCreated() throws Exception {
+        ProductDto mockDto = createTestProduct();
+        Mockito.when(productCrudService.createProduct(any(Product.class)))
+                .thenReturn(this.modelMapper.map(mockDto, Product.class));
+
+        mockMvc.perform(post(API_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.jsonMapper.writeValueAsString(new Product()))
+                ).andExpect(status().isCreated())
+                .andExpect(content().json(jsonMapper.writeValueAsString(mockDto)));
+    }
+
+    /**
+     * Check if a {@link Product} can be read.
+     */
+    @Test
+    void productCanBeRead() throws Exception {
+        ProductDto mockDto = createTestProduct();
+        Mockito.when(productCrudService.readProduct(any(int.class)))
+                .thenReturn(Optional.of(this.modelMapper.map(mockDto, Product.class)));
+
+        mockMvc.perform(get(API_PATH + "/" + mockDto.getId()))
+                .andExpect(status().isOk())
+                .andExpect(content().json(jsonMapper.writeValueAsString(mockDto)));
+    }
+
+    /**
+     * Check if a {@link Product} can be updated.
+     */
+    @Test
+    void productCanBeUpdated() throws Exception {
+        ProductDto mockDto = createTestProduct();
+        Mockito.when(productCrudService.updateProduct(argThat(argument -> argument.getId() == mockDto.getId())))
+                .thenReturn(this.modelMapper.map(mockDto, Product.class));
+
+        mockMvc.perform(put(API_PATH + "/" + mockDto.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(this.jsonMapper.writeValueAsString(mockDto))
+                ).andExpect(status().isOk())
+                .andExpect(content().json(jsonMapper.writeValueAsString(mockDto)));
+    }
+
+    /**
+     * Check if a {@link Product} can be deleted.
+     */
+    @Test
+    void productCanBeDeleted() throws Exception {
+        mockMvc.perform(delete(API_PATH + "/1"))
+                .andExpect(status().is(HttpStatus.NO_CONTENT.value()));
+    }
+
+    private ProductDto createTestProduct() {
+        Random random = new Random();
+        ProductDto dto = new ProductDto();
+        dto.setSerial(UUID.randomUUID().toString());
+        dto.setName(UUID.randomUUID().toString().replace("-", "").substring(10));
+        dto.setHeight(1 + (100 - 1) * random.nextDouble());
+        dto.setWidth(1 + (100 - 1) * random.nextDouble());
+        dto.setDepth(1 + (100 - 1) * random.nextDouble());
+        dto.setWeight(1 + (100 - 1) * random.nextDouble());
+        dto.setPrice(1 + (100 - 1) * random.nextDouble());
+        return dto;
+    }
+}
