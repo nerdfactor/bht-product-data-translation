5e0507e732fc30d12659599ba4ee0c201d340581
Daniel Klug <mail@nerdfactor.eu>
2023-12-29 11:02:09 +0100

    added CORS config for local angular dev server.

--- backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/config/ApiConfig.java
+++ backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/config/ApiConfig.java
@@ -4,24 +4,42 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Basic Api configuration.
  */
 @Configuration
-public class ApiConfig {
+public class ApiConfig implements WebMvcConfigurer {
 
-    /**
-     * Provides a bean of {@link ModelMapper} to map between models.
-     *
-     * @return A new ModelMapper
-     */
-    @Bean
-    public ModelMapper getModelMapper() {
-        ModelMapper mapper = new ModelMapper();
-        mapper.getConfiguration()
-                .setSkipNullEnabled(true)
-                .setMatchingStrategy(MatchingStrategies.STRICT);
-        return mapper;
-    }
+	/**
+	 * Provides a bean of {@link ModelMapper} to map between models.
+	 *
+	 * @return A new ModelMapper
+	 */
+	@Bean
+	public ModelMapper getModelMapper() {
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration()
+				.setSkipNullEnabled(true)
+				.setMatchingStrategy(MatchingStrategies.STRICT);
+		return mapper;
+	}
+
+	/**
+	 * Add CORS exceptions for localhost on 4200 so that angular development server
+	 * can access the api.
+	 *
+	 * @param registry The altered CORS Registry.
+	 */
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:4200")
+				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "CONNECT", "OPTIONS")
+				.allowedHeaders("*")
+				.exposedHeaders("*")
+				.allowCredentials(true).maxAge(3600);
+	}
 }
