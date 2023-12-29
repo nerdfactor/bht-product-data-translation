89e7e16a9debedcac210282e5dbed35b101ee7a1
Daniel Klug <mail@nerdfactor.eu>
2023-12-29 10:57:55 +0100

    had to change jpa annotation again because h2 and mysql db don't work like postgres.

--- backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/entity/Translation.java
+++ backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/entity/Translation.java
@@ -20,98 +20,98 @@ import java.util.Set;
 @NoArgsConstructor
 public class Translation {
 
-    /**
-     * An internal identifier for the {@link Translation}.
-     * The id will be automatically incremented by the database.
-     */
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	/**
+	 * An internal identifier for the {@link Translation}.
+	 * The id will be automatically incremented by the database.
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    /**
-     * A short description for the {@link Product}.
-     */
-    private String shortDescription;
+	/**
+	 * A short description for the {@link Product}.
+	 */
+	private String shortDescription;
 
-    /**
-     * A longer description for the {@link Product}.
-     */
-    @Column(columnDefinition = "text")
-    private String longDescription;
+	/**
+	 * A longer description for the {@link Product}.
+	 */
+    @Column(length = 3000)
+	private String longDescription;
 
-    /**
-     * The {@link Product} this {@link Translation} is for.
-     */
-    @ManyToOne
-    @JoinColumn(name = "product_id")
-    private Product product;
+	/**
+	 * The {@link Product} this {@link Translation} is for.
+	 */
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
 
-    /**
-     * Multiple {@link Revision Revisions} for this {@link Translation}.
-     */
-    @OneToMany(mappedBy = "translation", cascade = {CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.MERGE})
-    private Set<Revision> revisions;
+	/**
+	 * Multiple {@link Revision Revisions} for this {@link Translation}.
+	 */
+	@OneToMany(mappedBy = "translation", cascade = {CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.MERGE})
+	private Set<Revision> revisions;
 
-    /**
-     * The {@link Language} for this {@link Translation}.
-     */
-    @ManyToOne
-    @JoinColumn(name = "language_id")
-    private Language language;
+	/**
+	 * The {@link Language} for this {@link Translation}.
+	 */
+	@ManyToOne
+	@JoinColumn(name = "language_id")
+	private Language language;
 
-    /**
-     * Basic constructor with all data fields in order to create new {@link Translation Translations}.
-     * The id will be set during creation of the object in the database.
-     */
-    public Translation(String shortDescription, String longDescription) {
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
-    }
+	/**
+	 * Basic constructor with all data fields in order to create new {@link Translation Translations}.
+	 * The id will be set during creation of the object in the database.
+	 */
+	public Translation(String shortDescription, String longDescription) {
+		this.shortDescription = shortDescription;
+		this.longDescription = longDescription;
+	}
 
-    public Translation(String shortDescription, String longDescription, Language language) {
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
-        this.language = language;
-    }
+	public Translation(String shortDescription, String longDescription, Language language) {
+		this.shortDescription = shortDescription;
+		this.longDescription = longDescription;
+		this.language = language;
+	}
 
-    /**
-     * Add a {@link Revision}.
-     *
-     * @param revision The {@link Revision} to add.
-     */
-    public void addRevision(@NotNull Revision revision) {
-        if (this.revisions == null) {
-            this.revisions = new HashSet<>();
-        }
-        this.revisions.add(revision);
-    }
+	/**
+	 * Add a {@link Revision}.
+	 *
+	 * @param revision The {@link Revision} to add.
+	 */
+	public void addRevision(@NotNull Revision revision) {
+		if (this.revisions == null) {
+			this.revisions = new HashSet<>();
+		}
+		this.revisions.add(revision);
+	}
 
-    /**
-     * Remove a {@link Revision}.
-     *
-     * @param revision The {@link Revision} to remove.
-     */
-    public void removeRevision(@NotNull Revision revision) {
-        if (this.revisions == null) {
-            this.revisions = new HashSet<>();
-        }
-        this.revisions.remove(revision);
-    }
+	/**
+	 * Remove a {@link Revision}.
+	 *
+	 * @param revision The {@link Revision} to remove.
+	 */
+	public void removeRevision(@NotNull Revision revision) {
+		if (this.revisions == null) {
+			this.revisions = new HashSet<>();
+		}
+		this.revisions.remove(revision);
+	}
 
-    /**
-     * Compare an Object to this {@link Translation} by checking
-     * object equality or the same id.
-     *
-     * @param o The Object to compare.
-     * @return true if Object is equal to this {@link Translation}.
-     */
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Translation obj = (Translation) o;
-        return o == this || this.id == obj.id;
-    }
+	/**
+	 * Compare an Object to this {@link Translation} by checking
+	 * object equality or the same id.
+	 *
+	 * @param o The Object to compare.
+	 * @return true if Object is equal to this {@link Translation}.
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Translation obj = (Translation) o;
+		return o == this || this.id == obj.id;
+	}
 
 }
