da3f4037a98fab72955d22e5f8ef55f46edbca60
Daniel Klug <mail@nerdfactor.eu>
2023-12-30 17:42:54 +0100

    changed the controllers to return proper DTOs and throw exceptions instead of returning different types of objects. this is in order to catch the DTOs in the AutoTranslateAdvice.

--- backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/api/LanguageRestController.java
+++ backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/api/LanguageRestController.java
@@ -1,8 +1,9 @@
 package de.bhtberlin.paf2023.productdatatranslation.api;
 
-import de.bhtberlin.paf2023.productdatatranslation.dto.ErrorResponseDto;
 import de.bhtberlin.paf2023.productdatatranslation.dto.LanguageDto;
 import de.bhtberlin.paf2023.productdatatranslation.entity.Language;
+import de.bhtberlin.paf2023.productdatatranslation.exception.EntityNotFoundException;
+import de.bhtberlin.paf2023.productdatatranslation.exception.UnprocessableEntityException;
 import de.bhtberlin.paf2023.productdatatranslation.service.LanguageCrudService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -11,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Controller for {@link Language} related REST operations.
@@ -21,56 +21,47 @@ import java.util.Optional;
 @RequestMapping("/api/languages")
 public class LanguageRestController {
 
-    final LanguageCrudService languageCrudService;
+	final LanguageCrudService languageCrudService;
 
-    final ModelMapper mapper;
+	final ModelMapper mapper;
 
-    @GetMapping(value = {"", "/"})
-    public ResponseEntity<List<LanguageDto>> listAllLanguages() {
-        return ResponseEntity.ok(this.languageCrudService.listAllLanguages()
-                .stream().map(language -> this.mapper.map(language, LanguageDto.class))
-                .toList());
-    }
+	@GetMapping(value = {"", "/"})
+	public ResponseEntity<List<LanguageDto>> listAllLanguages() {
+		return ResponseEntity.ok(this.languageCrudService.listAllLanguages()
+				.stream().map(language -> this.mapper.map(language, LanguageDto.class))
+				.toList());
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> readLanguage(@PathVariable final int id) {
-        Optional<Language> language = this.languageCrudService.readLanguage(id);
-        if (language.isEmpty()) {
-            return ErrorResponseDto.createResponseEntity(
-                    HttpStatus.NOT_FOUND,
-                    "Language with Id " + id + " was not found."
-            );
-        }
-        return ResponseEntity.ok(this.mapper.map(language.get(), LanguageDto.class));
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<LanguageDto> readLanguage(@PathVariable final int id) {
+		Language language = this.languageCrudService.readLanguage(id).
+				orElseThrow(() -> new EntityNotFoundException("Language with Id " + id + " was not found."));
+		return ResponseEntity.ok(this.mapper.map(language, LanguageDto.class));
+	}
 
-    /**
-     * Create a new {@link Language} .
-     * <p>
-     * This method will enforce plain {@link Language} creation by removing all linked entities.
-     */
-    @PostMapping("")
-    public ResponseEntity<LanguageDto> createLanguage(@RequestBody final LanguageDto dto) {
-        dto.setTranslations(null);
-        Language created = this.languageCrudService.createLanguage(this.mapper.map(dto, Language.class));
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.mapper.map(created, LanguageDto.class));
-    }
+	/**
+	 * Create a new {@link Language} .
+	 * <p>
+	 * This method will enforce plain {@link Language} creation by removing all linked entities.
+	 */
+	@PostMapping("")
+	public ResponseEntity<LanguageDto> createLanguage(@RequestBody final LanguageDto dto) {
+		Language created = this.languageCrudService.createLanguage(this.mapper.map(dto, Language.class));
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.mapper.map(created, LanguageDto.class));
+	}
 
-    @RequestMapping(value = "/{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
-    public ResponseEntity<?> setLanguage(@PathVariable final int id, @RequestBody final LanguageDto dto) {
-        if (id != dto.getId()) {
-            return ErrorResponseDto.createResponseEntity(
-                    HttpStatus.UNPROCESSABLE_ENTITY,
-                    "Mismatch between provided Ids."
-            );
-        }
-        Language updated = this.languageCrudService.updateLanguage(this.mapper.map(dto, Language.class));
-        return ResponseEntity.ok(this.mapper.map(updated, LanguageDto.class));
-    }
+	@RequestMapping(value = "/{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
+	public ResponseEntity<LanguageDto> setLanguage(@PathVariable final int id, @RequestBody final LanguageDto dto) {
+		if (id != dto.getId()) {
+			throw new UnprocessableEntityException(String.format("Mismatch between provided Ids (%d - %d).", id, dto.getId()));
+		}
+		Language updated = this.languageCrudService.updateLanguage(this.mapper.map(dto, Language.class));
+		return ResponseEntity.ok(this.mapper.map(updated, LanguageDto.class));
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteLanguage(@PathVariable final int id) {
-        this.languageCrudService.deleteLanguageById(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteLanguage(@PathVariable final int id) {
+		this.languageCrudService.deleteLanguageById(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 }
--- backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/api/PictureRestController.java
+++ backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/api/PictureRestController.java
@@ -1,8 +1,9 @@
 package de.bhtberlin.paf2023.productdatatranslation.api;
 
-import de.bhtberlin.paf2023.productdatatranslation.dto.ErrorResponseDto;
 import de.bhtberlin.paf2023.productdatatranslation.dto.PictureDto;
 import de.bhtberlin.paf2023.productdatatranslation.entity.Picture;
+import de.bhtberlin.paf2023.productdatatranslation.exception.EntityNotFoundException;
+import de.bhtberlin.paf2023.productdatatranslation.exception.UnprocessableEntityException;
 import de.bhtberlin.paf2023.productdatatranslation.service.PictureCrudService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -11,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  * Controller for {@link Picture} related REST operations.
@@ -21,56 +21,48 @@ import java.util.Optional;
 @RequestMapping("/api/pictures")
 public class PictureRestController {
 
-    final PictureCrudService pictureCrudService;
+	final PictureCrudService pictureCrudService;
 
-    final ModelMapper mapper;
+	final ModelMapper mapper;
 
-    @GetMapping(value = {"", "/"})
-    public ResponseEntity<List<PictureDto>> listAllPictures() {
-        return ResponseEntity.ok(this.pictureCrudService.listAllPictures()
-                .stream().map(picture -> this.mapper.map(picture, PictureDto.class))
-                .toList());
-    }
+	@GetMapping(value = {"", "/"})
+	public ResponseEntity<List<PictureDto>> listAllPictures() {
+		return ResponseEntity.ok(this.pictureCrudService.listAllPictures()
+				.stream().map(picture -> this.mapper.map(picture, PictureDto.class))
+				.toList());
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> readPicture(@PathVariable final int id) {
-        Optional<Picture> picture = this.pictureCrudService.readPicture(id);
-        if (picture.isEmpty()) {
-            return ErrorResponseDto.createResponseEntity(
-                    HttpStatus.NOT_FOUND,
-                    "Picture with Id " + id + " was not found."
-            );
-        }
-        return ResponseEntity.ok(this.mapper.map(picture.get(), PictureDto.class));
-    }
+	@GetMapping("/{id}")
+	public ResponseEntity<PictureDto> readPicture(@PathVariable final int id) {
+		Picture picture = this.pictureCrudService.readPicture(id).
+				orElseThrow(() -> new EntityNotFoundException("Picture with Id " + id + " was not found."));
+		return ResponseEntity.ok(this.mapper.map(picture, PictureDto.class));
+	}
 
-    /**
-     * Create a new {@link Picture} .
-     * <p>
-     * This method will enforce plain {@link Picture} creation by removing all linked entities.
-     */
-    @PostMapping("")
-    public ResponseEntity<PictureDto> createPicture(@RequestBody final PictureDto dto) {
-        dto.setProduct(null);
-        Picture created = this.pictureCrudService.createPicture(this.mapper.map(dto, Picture.class));
-        return ResponseEntity.status(HttpStatus.CREATED).body(this.mapper.map(created, PictureDto.class));
-    }
+	/**
+	 * Create a new {@link Picture} .
+	 * <p>
+	 * This method will enforce plain {@link Picture} creation by removing all linked entities.
+	 */
+	@PostMapping("")
+	public ResponseEntity<PictureDto> createPicture(@RequestBody final PictureDto dto) {
+		dto.setProduct(null);
+		Picture created = this.pictureCrudService.createPicture(this.mapper.map(dto, Picture.class));
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.mapper.map(created, PictureDto.class));
+	}
 
-    @RequestMapping(value = "/{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
-    public ResponseEntity<?> setPicture(@PathVariable final int id, @RequestBody final PictureDto dto) {
-        if (id != dto.getId()) {
-            return ErrorResponseDto.createResponseEntity(
-                    HttpStatus.UNPROCESSABLE_ENTITY,
-                    "Mismatch between provided Ids."
-            );
-        }
-        Picture updated = this.pictureCrudService.updatePicture(this.mapper.map(dto, Picture.class));
-        return ResponseEntity.ok(this.mapper.map(updated, PictureDto.class));
-    }
+	@RequestMapping(value = "/{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
+	public ResponseEntity<PictureDto> setPicture(@PathVariable final int id, @RequestBody final PictureDto dto) {
+		if (id != dto.getId()) {
+			throw new UnprocessableEntityException(String.format("Mismatch between provided Ids (%d - %d).", id, dto.getId()));
+		}
+		Picture updated = this.pictureCrudService.updatePicture(this.mapper.map(dto, Picture.class));
+		return ResponseEntity.ok(this.mapper.map(updated, PictureDto.class));
+	}
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> deletePicture(@PathVariable final int id) {
-        this.pictureCrudService.deletePictureById(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletePicture(@PathVariable final int id) {
+		this.pictureCrudService.deletePictureById(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 }
