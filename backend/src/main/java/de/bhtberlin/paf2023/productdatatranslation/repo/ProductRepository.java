1b4f46d003fbd0366a5e770d9b685d945f4ed1f4
Daniel Klug <mail@nerdfactor.eu>
2024-01-02 13:29:15 +0100

    add basic search in products.

--- backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/repo/ProductRepository.java
+++ backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/repo/ProductRepository.java
@@ -1,10 +1,53 @@
 package de.bhtberlin.paf2023.productdatatranslation.repo;
 
 import de.bhtberlin.paf2023.productdatatranslation.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer>, JpaSpecificationExecutor<Product> {
+
+    /**
+     * Search products containing a search term in
+     * - name
+     * - Translations shortDescription
+     * - Translations longDescription
+     *
+     * @param search   The search term.
+     * @param pageable The {@link Pageable} restricting the amount of results.
+     * @return A {@link Page} containing the results.
+     */
+    @Query("SELECT product FROM Product product " +
+            "LEFT JOIN Translation translation ON translation.product = product " +
+            "WHERE lower(product.name) LIKE :search " +
+            "OR lower(translation.shortDescription) LIKE :search " +
+            "OR lower(translation.longDescription) LIKE :search")
+    Page<Product> searchAllProducts(String search, Pageable pageable);
+
+    /**
+     * Search products containing a search term in
+     * - name
+     * - Translations shortDescription
+     * - Translations longDescription
+     * Takes an additional search term that is translated in the default language
+     * in order to find products that are not translated yet.
+     *
+     * @param search                The search term.
+     * @param defaultLanguageSearch The search term translated in the default language.
+     * @param pageable              The {@link Pageable} restricting the amount of results.
+     * @return A {@link Page} containing the results.
+     */
+    @Query("SELECT product FROM Product product " +
+            "LEFT JOIN Translation translation ON translation.product = product " +
+            "WHERE lower(product.name) LIKE :search " +
+            "OR lower(translation.shortDescription) LIKE :search " +
+            "OR lower(translation.longDescription) LIKE :search " +
+            "OR lower(product.name) LIKE :defaultLanguageSearch " +
+            "OR lower(translation.shortDescription) LIKE :defaultLanguageSearch " +
+            "OR lower(translation.longDescription) LIKE :defaultLanguageSearch")
+    Page<Product> searchAllProductsMultiSearch(String search, String defaultLanguageSearch, Pageable pageable);
 }
--- backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/service/ProductSearchService.java
+++ backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/service/ProductSearchService.java
@@ -1,5 +1,6 @@
 package de.bhtberlin.paf2023.productdatatranslation.service;
 
+import de.bhtberlin.paf2023.productdatatranslation.config.AppConfig;
 import de.bhtberlin.paf2023.productdatatranslation.entity.Product;
 import de.bhtberlin.paf2023.productdatatranslation.repo.ProductRepository;
 import lombok.NonNull;
@@ -7,11 +8,13 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
 
+/**
+ * Service for product search.
+ */
 @Slf4j
 @RequiredArgsConstructor
 @Service
@@ -25,18 +28,26 @@ public class ProductSearchService {
      */
     final ProductRepository productRepository;
 
-    public @NonNull Page<Product> searchAllProducts(final String search) {
-        return this.searchAllProducts(search, "");
+    public @NonNull Page<Product> searchAllProducts(String search, Pageable page) {
+        return this.searchAllProducts(search, "", page);
     }
 
-    public @NonNull Page<Product> searchAllProducts(final String search, Locale locale) {
-        return this.searchAllProducts(search, locale.toLanguageTag());
+    public @NonNull Page<Product> searchAllProducts(String search, Locale locale, Pageable page) {
+        return this.searchAllProducts(search, locale.toLanguageTag(), page);
     }
 
-    public @NonNull Page<Product> searchAllProducts(final String search, String locale) {
-        Specification<Product> spec = Specification.where(null);
-        spec.and((root, query, cb) -> cb.like(root.get("name"), search));
-        Page<Product> products = this.productRepository.findAll(spec, Pageable.unpaged());
+    /**
+     * Search all products containing the search term.
+     *
+     * @param search The search term.
+     * @param locale The locale that the result will be restricted to.
+     * @return A {@link Page} with the search results.
+     */
+    public @NonNull Page<Product> searchAllProducts(String search, String locale, Pageable page) {
+        String defaultLanguageSearch = this.translationService.translate(search, locale, AppConfig.DEFAULT_LANGUAGE);
+        defaultLanguageSearch = "%" + defaultLanguageSearch.trim().toLowerCase() + "%";
+        search = "%" + search.trim().toLowerCase() + "%";
+        Page<Product> products = this.productRepository.searchAllProductsMultiSearch(search, defaultLanguageSearch, page);
         products.getContent().forEach(product -> {
             product.removeTranslationsNotInLocale(locale);
             if (!product.hasTranslations()) {
