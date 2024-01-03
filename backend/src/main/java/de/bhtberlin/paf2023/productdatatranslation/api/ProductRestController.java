a0b7028f415e76691a3663d481e67ca2e0a8998b
Daniel Klug <mail@nerdfactor.eu>
2024-01-03 18:43:21 +0100

    separate PUT and PATCH endpoints in order to handle updates of relationships.

--- backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/api/ProductRestController.java
+++ backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/api/ProductRestController.java
@@ -1,6 +1,11 @@
 package de.bhtberlin.paf2023.productdatatranslation.api;
 
+import com.google.cloud.translate.Translation;
+import de.bhtberlin.paf2023.productdatatranslation.dto.CategoryDto;
+import de.bhtberlin.paf2023.productdatatranslation.dto.ColorDto;
 import de.bhtberlin.paf2023.productdatatranslation.dto.ProductDto;
+import de.bhtberlin.paf2023.productdatatranslation.entity.Category;
+import de.bhtberlin.paf2023.productdatatranslation.entity.Color;
 import de.bhtberlin.paf2023.productdatatranslation.entity.Product;
 import de.bhtberlin.paf2023.productdatatranslation.exception.EntityNotFoundException;
 import de.bhtberlin.paf2023.productdatatranslation.exception.UnprocessableEntityException;
@@ -12,7 +17,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * Controller for {@link Product} related REST operations.
@@ -55,10 +63,53 @@ public class ProductRestController {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.mapper.map(created, ProductDto.class));
     }
 
-    @RequestMapping(value = "/{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
+    /**
+     * Set a {@link Product} to the data in the DTO. This will override the data
+     * of the existing entity. So make sure the DTO contains everything.
+     * <br>
+     * Update connected {@link Translation Translations} from other side of relationship.
+     *
+     * @param id  The id of the {@link Product}.
+     * @param dto The {@link ProductDto} containing the new data.
+     * @return The updated {@link ProductDto}.
+     */
+    @PutMapping(value = "/{id}")
     public ResponseEntity<ProductDto> setProduct(@PathVariable final int id, @RequestBody final ProductDto dto) {
         if (id != dto.getId()) {
             throw new UnprocessableEntityException(String.format("Mismatch between provided Ids (%d - %d).", id, dto.getId()));
+        }
+        Product updated = this.productCrudService.updateProduct(this.mapper.map(dto, Product.class));
+        return ResponseEntity.ok(this.mapper.map(updated, ProductDto.class));
+    }
+
+    /**
+     * Update a {@link Product} with the data in the DTO. It will merge the data
+     * with the existing entity.
+     * <br>
+     * If the DTO contains now lists for {@link Category Categories} or {@link Color Colors}
+     * they will be filled with the existing relationships. To reset the relationships
+     * send an empty array (to clear all) or an array containing all the desired
+     * relations.
+     * <br>
+     * Update connected {@link Translation Translations} from other side of relationship.
+     *
+     * @param id  The id of the {@link Product}.
+     * @param dto The {@link ProductDto} containing the updated data.
+     * @return The updated {@link ProductDto}.
+     */
+    @PatchMapping(value = "/{id}")
+    public ResponseEntity<ProductDto> updateProduct(@PathVariable final int id, @RequestBody final ProductDto dto) {
+        if (id != dto.getId()) {
+            throw new UnprocessableEntityException(String.format("Mismatch between provided Ids (%d - %d).", id, dto.getId()));
+        }
+        Product entity = this.productCrudService.readProduct(dto.getId()).orElseThrow(() -> new EntityNotFoundException("Product with Id " + id + " was not found."));
+        if (dto.getCategories() == null) {
+            // if categories where not send, fill with existing relations.
+            dto.setCategories(Optional.ofNullable(entity.getCategories()).orElse(new HashSet<>()).stream().map(c -> new CategoryDto(c.getId())).collect(Collectors.toSet()));
+        }
+        if (dto.getColors() == null) {
+            // if colors where not send, fill with existing relations.
+            dto.setColors(Optional.ofNullable(entity.getColors()).orElse(new HashSet<>()).stream().map(c -> new ColorDto(c.getId())).collect(Collectors.toSet()));
         }
         Product updated = this.productCrudService.updateProduct(this.mapper.map(dto, Product.class));
         return ResponseEntity.ok(this.mapper.map(updated, ProductDto.class));
