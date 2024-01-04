89797a4cf8b1aa941a2f1a6bfe45f26b2f791f95
Daniel Klug <mail@nerdfactor.eu>
2024-01-04 09:18:02 +0100

    add test for "ShowProduct" use case.

---
+++ backend/src/test/java/de/bhtberlin/paf2023/productdatatranslation/usecase/ShowProductTest.java
@@ -0,0 +1,72 @@
+package de.bhtberlin.paf2023.productdatatranslation.usecase;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.bhtberlin.paf2023.productdatatranslation.dto.ProductDto;
+import de.bhtberlin.paf2023.productdatatranslation.dto.TranslationDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Locale;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("integration")
+public class ShowProductTest {
+
+    private static final String API_PATH = "/api/products";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper jsonMapper;
+
+    /**
+     * Should return product details in an existing language.
+     */
+    @Test
+    @Transactional
+    public void shouldReturnProductInExistingLanguage() throws Exception {
+        int id = 1;
+        String response = mockMvc.perform(get(API_PATH + "/" + id)
+                        .header("Accept-Language", Locale.GERMAN))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        ProductDto responseDto = this.jsonMapper.readValue(response, ProductDto.class);
+        Assertions.assertEquals(id, responseDto.getId());
+    }
+
+    /**
+     * Should return product details in a non-existing language.
+     */
+    @Test
+    @Transactional
+    public void shouldReturnProductInNonExistingLanguage() throws Exception {
+        int id = 1;
+        String response = mockMvc.perform(get(API_PATH + "/" + id)
+                        .header("Accept-Language", Locale.FRENCH))
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        ProductDto responseDto = this.jsonMapper.readValue(response, ProductDto.class);
+        Assertions.assertEquals(id, responseDto.getId());
+        Assertions.assertTrue(responseDto.getTranslations().size() > 1);
+        boolean hasCorrectTranslation = false;
+        for (TranslationDto translation : responseDto.getTranslations()) {
+            if (translation.getLanguage().getIsoCode().equalsIgnoreCase(Locale.FRENCH.toLanguageTag())) {
+                hasCorrectTranslation = true;
+            }
+        }
+        Assertions.assertTrue(hasCorrectTranslation);
+    }
+}
