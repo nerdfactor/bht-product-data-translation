17682299039fa2cb62f9ddf28820c1f09dc13af4
Jeffrey King <jk95@hotmail.de>
2024-01-04 14:58:47 +0100

    adds form functionality in edit page

--- frontend/ProductDataTranslation/src/app/components/edit-page/edit-page.component.ts
+++ frontend/ProductDataTranslation/src/app/components/edit-page/edit-page.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Product } from '../../models/product';
+import { Translation } from '../../models/translation';
+import { Language } from '../../models/language';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-page',
@@ -7,6 +10,9 @@ import { Product } from '../../models/product';
   styleUrl: './edit-page.component.scss'
 })
 export class EditPageComponent {
+
+  constructor(private formBuilder: FormBuilder) { }
+
   product: Product = {
     id: 0,
     name: '',
@@ -21,4 +27,44 @@ export class EditPageComponent {
     weight: 0,
     width: 0
   };
+
+  language: Language = {
+    id: 0,
+    currency: '',
+    isoCode: '',
+    measure: '',
+    name: '',
+    translations: []
+  }
+
+  translation: Translation = {
+    id: 0,
+    longDescription: '',
+    shortDescription: '',
+    product: this.product,
+    language: this.language
+  }
+
+  productForm = this.formBuilder.group({
+    name: [''],
+    depth: [0],
+    height: [0],
+    price: [0],
+    serial: [''],
+    weight: [0],
+    width: [0],
+    shortDescription: [''],
+    longDescription: ['']
+  })
+
+  onSubmit() {
+    this.product.name = this.productForm.value.name!;
+    this.product.depth = this.productForm.value.depth!;
+    this.product.height = this.productForm.value.height!;
+    this.product.price = this.productForm.value.price!;
+    this.product.serial = this.productForm.value.serial!;
+    this.product.weight = this.productForm.value.weight!;
+    this.product.width = this.productForm.value.width!;
+    console.log('submitting', this.product);
+  }
 }
