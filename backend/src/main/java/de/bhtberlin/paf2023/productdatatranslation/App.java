fdad54505f90f566774e659d990ae172da254a55
Daniel Klug <mail@nerdfactor.eu>
2024-01-11 13:34:29 +0100

    enable async in application.

--- backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/App.java
+++ backend/src/main/java/de/bhtberlin/paf2023/productdatatranslation/App.java
@@ -2,11 +2,13 @@ package de.bhtberlin.paf2023.productdatatranslation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * Basic Spring Boot Application.
  */
 @SpringBootApplication
+@EnableAsync
 public class App {
 
     public static void main(String[] args) {
